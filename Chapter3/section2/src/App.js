777e7672f14ed8afefce831a544960a36462033b
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2020-03-07 19:50:31 +0900

    finshed Chapter3-2.2

--- Chapter3/section2/src/App.js
+++ Chapter3/section2/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Chapter3-section2-2
+class App extends Component{
+  constructor(props){
+    super();
+    this.title = props.title;
+    this.message = props.message;
+  }
+  render(){
+    return(
+      <div className="App">
+        <h1>{this.title}</h1>
+        <p>{this.message}</p>
+      </div>
+    )
+  }
 }
 
 export default App;
+
+// Chapter3-section2-1
+// class App extends Component {
+//   render(){
+//     return (
+//       <div className="App">
+//         <h1>React</h1>
+//         <p>This is sample Component</p>
+//         <p>これはサンプルのコンポーネントです</p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
