f9fff0003e3d5209741061cc5a57548a2722a9eb
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2020-03-28 11:49:52 +0900

    setting index.js

---
+++ chapter4/section1/src/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// satateの値
+let state_value ={
+  counter: 0,
+  message: "COUNTER"
+}
+
+//reducer
+function counter(state = state_value, action){
+  switch (action.type){
+    case 'INCREMENT':
+      return{
+        counter: state.counter + 1,
+        message: 'INCREMENT'
+      };
+    case 'DECREMENT':
+      return{
+        counter: state.counter - 1,
+        message: 'DECREMENT'
+      };
+    default:
+      return state;
+  }
+}
+
+// create store
+let store = createStore(counter);
+
+// 表示をレンダリング
+ReactDOM.render(
+  <Provider store = {store}>
+    <App />
+  </Provider>
+  document.getElementById('root')
+);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
