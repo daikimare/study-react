ab8037a17aeaa4cbaa955eca06e8e9fc0e555af1
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2020-04-02 11:26:43 +0900

    up date file

--- chapter4/section3/src/index.js
+++ chapter4/section3/src/index.js
@@ -1,17 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { persistStore,persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
+// stateの値
+let state_value ={
+  counter: 0,
+  message: "COUNTER"
+}
+
+// reducer
+function counter (state = state_value,action){
+  switch (action.type){
+    case 'INCREMENT':
+      return {
+        counter: state.counter + 1,
+        message: "INCREMENT"
+      };
+    case 'DECREMENT':
+      return {
+        counter: state.counter - 1,
+        message: "DECREMENT"
+      };
+    case 'RESET':
+      return {
+        counter: 0,
+        message: "RESET"
+      };
+    default:
+      return state;
+  }
+}
+
+// Redux Persistnの設定
+const persistConfig ={
+  key: 'root',
+  storage,
+}
+
+// create persist reducer
+const persistedReducer = persistReducer(persistConfig, counter)
+
+// create store and persister
+let store = createStore(persistedReducer)
+let pstore = persistStore(store)
+
+// 表示をレンダリング
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store ={store}>
+    <PersistGate loading ={<p>loading...</p>} persistore ={pstore}>
+      <App />
+    </PersistGate>
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
